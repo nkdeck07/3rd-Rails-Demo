b793a47ca0d2f85d66ff38d53fb1b10a46f9cc8c
Nancy <nancy@cantinaconsulting.com>
2012-06-06 15:24:33 -0400

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+
 
  #get "static_pages/home"
  #get "static_pages/help"
  #get "static_pages/about"
  #get "static_pages/contact"
 
+resources :users
+
 root to: 'static_pages#home'
 match '/help', to: 'static_pages#help'
 match '/about', to: 'static_pages#about'
 match '/contact', to: 'static_pages#contact'
 match '/signup', to: 'users#new'
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
